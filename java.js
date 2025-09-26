2632c69c78aabeaa05cda8bd6e06afc879f288aa
mks8102007-ai <mks8102007@gmail.com>
2025-09-26 21:09:43 +0530

    first commit

--- java.js
+++ java.js
@@ -1,21 +1,51 @@
-let name=prompt("Hi,What is your name?");
-let usage=prompt("how old are you?");
-userage=Number(userage);
-if(userage>=18)
-{
-  alert("Good"+name+"you are eligible for voting");
+function checkeligidlity(){
+  let username=prompt("whay is your name?");
+  let userage=prompt("how old are you?");
+  userage=Number(userage);
+  if (userage>=18){
+    alert("HELLO"+username+"you are not eligible to vote");
+  }
+  else{
+    alert("SORRY"+username+"you are not eligible to vote");
+  }
+  let again=confirm("Do you want to check again?");
+  if(again){
+    checkeligidlity();
+  }else{
+    alert("Good bye!")
+  }
 }
-else
+checkeligidlity();
+
+let username=prompt("enter your name")
+let password=prompt("enter your password")
+if(username=="mounika" && password=="book123")
 {
-  alert("sorry you are not eligible");
-}
-let final=confirm("Do you want to checkagain?")
-if(final=true)
-{
-  alert("repeat")
+  let borrow=confirm("do you want to borrow a book?")
+
+  if(borrow)
+  {
+    let category =prompt("select your type of book:\n fiction\n science\n history")
+
+    switch(category)
+    {
+      case"fiction":
+          alert("you selected fiction")
+          break;
+      case"science":
+          alert("you selected science")
+          break;
+      case"history":
+          alert("you selected history")
+          break;
+      default:
+          alert("invalid category")
+    }
+  }
+  else{
+    alert("May be next time! goodbye! buddy")
+  }
 }
 else{
-  alert("Good bye")
+  alert("invalied login!")
 }
-let username=prompt("Enter your name")
-let password=prompt("Enter your password")
